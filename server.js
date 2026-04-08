cea8e4980fa77ed5d7d0edb772085696ec9ffabd
NoelNinanSheri1307 <noelninansheri@gmail.com>
2026-04-08 23:38:22 +0530

    Noel Ninan Sheri | v2: Student Registration Feature Added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 let courses = [];
-
+let registrations = [];
 // Add Course API
 app.get('/add-course', (req, res) => {
     const course = req.query.course;
@@ -24,6 +24,24 @@ app.get('/courses', (req, res) => {
     res.json(courses);
 });
 
-app.listen(3000, () => {
+// Register Student API (v2)
+app.get('/register', (req, res) => {
+    const student = req.query.student;
+    const course = req.query.course;
+
+    if (!student || !course) {
+        return res.send("Please provide both student and course");
+    }
+
+    registrations.push({ student, course });
+
+    res.send(`Student "${student}" registered for course "${course}"`);
+});
+
+app.get('/registrations', (req, res) => {
+    res.json(registrations);
+});
+
+app.listen(3000,'0.0.0.0',() => {
     console.log("Server running on port 3000");
 });
